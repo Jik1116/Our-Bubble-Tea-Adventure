f33c9c327ddba2cb0614a0d11c242030f63689ed
Isaac Tay <IsaacTay@pm.me>
2025-02-09 22:34:29 +0800

    chore: minor refactoring

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-
     [Header("Run")]
     public float forwardSpeed = 10.0f;
     public float maxSpeedX = 20.0f;
@@ -12,33 +10,29 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jump")]
     public float jumpForce = 400.0f;
+    public float gravityScale = 3.0f;
     public float jumpGravityMult = 0.7f;
     public float coyoteTime = 1.0f;
 
     [Header("Timers")]
-    [SerializeField] private float jumpPressedTime;
-    [SerializeField] private float onGroundTime;
+    public float onGroundTime;
+
+    [Header("Checks")]
+    [SerializeField] private float _groundCheckOffset = 0.27f;
+    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.33f, 0.03f);
+    [Header("Layers & Tags")]
+    [SerializeField] private LayerMask _groundLayer;
 
     private Vector2 directionalInput;
     private Rigidbody2D rb;
-
     private Vector2 force = Vector2.zero;
-    private float initalGravityScale;
-
     private bool jumpTrigger;
     private bool jumpCutTrigger;
-
-    [Header("Checks")]
-    [SerializeField] private Transform _groundCheckPoint;
-    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
-    [Header("Layers & Tags")]
-    [SerializeField] private LayerMask _groundLayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        initalGravityScale = rb.gravityScale;
     }
 
     // Update is called once per frame
@@ -61,11 +55,10 @@ public class PlayerMovement : MonoBehaviour
     {
         float deltaTime = Time.fixedDeltaTime;
         onGroundTime -= deltaTime;
-        jumpPressedTime -= deltaTime;
 
         if (rb.linearVelocityY < 0.0f)
         {
-            rb.gravityScale = initalGravityScale;
+            rb.gravityScale = gravityScale;
         }
 
         // Scale force based on how close we are to max speed
@@ -87,17 +80,17 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(force * Vector2.right);
         this.force.x = force;
 
-        if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer))
+        if (Physics2D.OverlapBox(_groundCheckPosition(), _groundCheckSize, 0, _groundLayer))
         {
             onGroundTime = coyoteTime;
         }
 
         if (jumpTrigger && onGroundTime > 0.0f)
         {
-            jumpPressedTime = onGroundTime = 0;
+            onGroundTime = 0;
 
             // Give player lower gravity when jump is held
-            rb.gravityScale = initalGravityScale * jumpGravityMult;
+            rb.gravityScale = gravityScale * jumpGravityMult;
 
             if (rb.linearVelocityY < 0)
             {
@@ -109,7 +102,7 @@ public class PlayerMovement : MonoBehaviour
         if (jumpCutTrigger)
         {
             // Revert gravity to original state for a jump cut
-            rb.gravityScale = initalGravityScale;
+            rb.gravityScale = gravityScale;
         }
 
         jumpTrigger = false;
@@ -120,6 +113,11 @@ public class PlayerMovement : MonoBehaviour
     {
         // Gizmos.DrawLine(transform.position, transform.position + new Vector3(directionalInput.x, directionalInput.y, 0.0f));
         Gizmos.DrawLine(transform.position, transform.position + force.x * Vector3.right);
-        Gizmos.DrawCube(_groundCheckPoint.position, _groundCheckSize);
+        Gizmos.DrawCube(_groundCheckPosition(), _groundCheckSize);
+    }
+
+    Vector3 _groundCheckPosition()
+    {
+        return transform.position + _groundCheckOffset * Vector3.down;
     }
 }
