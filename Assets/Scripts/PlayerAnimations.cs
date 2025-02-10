83840e4a60e07b1a4476e8a1a24631361f7da0e7
Isaac Tay <IsaacTay@pm.me>
2025-02-10 09:41:44 +0800

    feat: add animation scripts

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+
+    private PlayerMovement playerMovement;
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("onGround", playerMovement.onGroundTime > 0);
+        animator.SetFloat("xSpeed", Math.Abs(rb.linearVelocityX));
+    }
+}
